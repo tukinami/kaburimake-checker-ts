e26c4622e58067c2898daa04355825634ee5f7f3
tukinami seika <10forchette@gmail.com>
2024-12-05 10:15:17 +0900

    add: jsons of 4 events.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,6 @@
 export const ghostDataJsonList: string[] = [
 	'001.json',
+	'gayurigo.json',
 	'GhostMasquerade1.json',
 	'GhostMasquerade2.json',
 	'GhostMasquerade3.json',
@@ -7,8 +8,11 @@ export const ghostDataJsonList: string[] = [
 	'GhostMasquerade5.json',
 	'GhostMasquerade6.json',
 	'gsm.json',
+	'igyou-atama2024.json',
 	'Konuka.json',
+	'luckybag2024.json',
 	'mesghost.json',
 	'nantoka_site_pack.json',
+	'shirashiru.json',
 	'uenoeki.json'
 ];
